4e23bf9bfd4bec2520c977e26cdc82dfb61dc16b
Jonny Burger <jonathanburger11@gmail.com>
2022-11-23 16:31:09 +0100

    Update IssuesOpened.tsx

--- remotion/Languages/IssuesOpened.tsx
+++ remotion/Languages/IssuesOpened.tsx
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, {useMemo} from 'react';
 import {
 	AbsoluteFill,
 	interpolate,
+	interpolateColors,
 	spring,
 	useCurrentFrame,
 	useVideoConfig,
@@ -10,12 +11,75 @@ import {BG_2022} from '../../src/palette';
 import chunk from 'lodash.chunk';
 import {IssueCircle} from './IssueCircle';
 
-const issuesOpen = 1000;
-const issuesClosed = 100;
+const issuesOpen = 500;
+const issuesClosed = 1000;
 const padding = 30;
 const bottomSpace = 130;
 
 const duration = 100;
+
+const coordinateToAbsoluteIndex = (
+	coord: [number, number],
+	dotsPerRow: number
+) => {
+	return coord[1] * dotsPerRow + coord[0];
+};
+
+type Direction = 'left' | 'right';
+
+const getNextCoordLeft = (coord: [number, number]): [number, number] => {
+	if (coord[0] === 0 && coord[1] === 0) {
+		return [1, 0];
+	}
+	if (coord[0] === 0) {
+		return [coord[1] + 1, 0];
+	}
+
+	return [coord[0] - 1, coord[1] + 1];
+};
+
+const getNextCoordRight = (
+	coord: [number, number],
+	dotsPerRow: number
+): [number, number] => {
+	if (coord[0] === dotsPerRow - 1 && coord[1] === 0) {
+		return [dotsPerRow - 2, 0];
+	}
+	if (coord[0] === dotsPerRow - 1) {
+		return [dotsPerRow - coord[1] - 2, 0];
+	}
+
+	return [coord[0] + 1, coord[1] + 1];
+};
+
+const getNextCoord = (
+	coord: [number, number],
+	dotsPerRow: number,
+	direction: Direction
+) => {
+	if (direction === 'left') {
+		return getNextCoordLeft(coord);
+	}
+
+	return getNextCoordRight(coord, dotsPerRow);
+};
+
+const getColor = (
+	indicesToClose: number[],
+	index: number,
+	progress: number
+) => {
+	const indexOfIndex = indicesToClose.indexOf(index);
+	if (indexOfIndex === -1) {
+		return '#2da44e';
+	}
+	const position = indexOfIndex / (indicesToClose.length - 1);
+	return interpolateColors(
+		progress,
+		[position - 0.1, position],
+		['#2da44e', '#986ee2']
+	);
+};
 
 export const IssuesOpened2022: React.FC = () => {
 	const frame = useCurrentFrame();
@@ -31,6 +95,15 @@ export const IssuesOpened2022: React.FC = () => {
 			},
 			durationInFrames: duration,
 		}) * totalIssues;
+
+	const closedDotsProgress = spring({
+		fps,
+		frame: frame - 100,
+		config: {
+			damping: 200,
+		},
+		durationInFrames: duration,
+	});
 
 	const {height, width} = useVideoConfig();
 
@@ -53,6 +126,28 @@ export const IssuesOpened2022: React.FC = () => {
 		}),
 		dotsPerRow
 	);
+
+	// first to close from left: 0,
+	const indicesToClose = useMemo(() => {
+		let indices: number[] = [];
+
+		for (let i = 0; i < issuesClosed; i++) {
+			const direction = i % 2 === 0 ? 'left' : 'right';
+			let nextIndex: [number, number] =
+				direction === 'left' ? [0, 0] : [dotsPerRow - 1, 0];
+			let index = coordinateToAbsoluteIndex(nextIndex, dotsPerRow);
+
+			while (indices.includes(index)) {
+				nextIndex = getNextCoord(nextIndex, dotsPerRow, direction);
+				index = coordinateToAbsoluteIndex(nextIndex, dotsPerRow);
+				console.log({nextIndex, index});
+			}
+			indices.push(index);
+		}
+		return indices;
+	}, [dotsPerRow]);
+
+	console.log(indicesToClose);
 
 	return (
 		<AbsoluteFill
@@ -86,6 +181,11 @@ export const IssuesOpened2022: React.FC = () => {
 										extrapolateRight: 'clamp',
 									}
 								);
+								const color = getColor(
+									indicesToClose,
+									actualIndex,
+									closedDotsProgress
+								);
 								return (
 									<svg
 										key={i}
@@ -99,7 +199,7 @@ export const IssuesOpened2022: React.FC = () => {
 									>
 										{canAffordRoughJs ? (
 											<IssueCircle
-												fill="#2da44e"
+												fill={color}
 												r={50}
 												cx={50}
 												cy={50}
@@ -108,7 +208,7 @@ export const IssuesOpened2022: React.FC = () => {
 											></IssueCircle>
 										) : (
 											<circle
-												fill="#2da44e"
+												fill={color}
 												r={50}
 												cx={50}
 												cy={50}
