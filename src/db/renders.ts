8a6ad2d330ff8363909c88cc910045e06765c765
Jonny Burger <jonathanburger11@gmail.com>
2022-12-01 15:22:05 +0100

    wrapped2022 DB

--- src/db/renders.ts
+++ src/db/renders.ts
@@ -1,116 +1,116 @@
-import { AwsRegion } from "@remotion/lambda";
-import { WithId } from "mongodb";
-import { mongoClient } from "./mongo";
+import {AwsRegion} from '@remotion/lambda';
+import {WithId} from 'mongodb';
+import {mongoClient} from './mongo';
 
 export type Finality =
-  | {
-      type: "success";
-      url: string;
-    }
-  | {
-      type: "error";
-      errors: string;
-    };
+	| {
+			type: 'success';
+			url: string;
+	  }
+	| {
+			type: 'error';
+			errors: string;
+	  };
 
 export type Render = {
-  renderId: string | null;
-  region: AwsRegion;
-  username: string;
-  bucketName: string | null;
-  finality: Finality | null;
-  functionName: string;
-  account: number | undefined;
+	renderId: string | null;
+	region: AwsRegion;
+	username: string;
+	bucketName: string | null;
+	finality: Finality | null;
+	functionName: string;
+	account: number | undefined;
 };
 
 export const rendersCollection = async () => {
-  const client = await mongoClient;
-  return client.db("wrapped").collection<Render>("renders");
+	const client = await mongoClient;
+	return client.db('wrapped2022').collection<Render>('renders');
 };
 
 export const lockRender = async (
-  region: AwsRegion,
-  username: string,
-  account: number,
-  functionName: string
+	region: AwsRegion,
+	username: string,
+	account: number,
+	functionName: string
 ) => {
-  const coll = await rendersCollection();
-  await coll.insertOne({
-    region,
-    username: username.toLowerCase(),
-    bucketName: null,
-    finality: null,
-    renderId: null,
-    account: account,
-    functionName,
-  });
+	const coll = await rendersCollection();
+	await coll.insertOne({
+		region,
+		username: username.toLowerCase(),
+		bucketName: null,
+		finality: null,
+		renderId: null,
+		account: account,
+		functionName,
+	});
 };
 
 export const saveRender = async ({
-  region,
-  username,
-  renderId,
-  bucketName,
+	region,
+	username,
+	renderId,
+	bucketName,
 }: {
-  region: AwsRegion;
-  username: string;
-  renderId: string;
-  bucketName: string;
+	region: AwsRegion;
+	username: string;
+	renderId: string;
+	bucketName: string;
 }) => {
-  const coll = await rendersCollection();
-  await coll.updateOne(
-    {
-      region,
-      username: username.toLowerCase(),
-    },
-    {
-      $set: {
-        renderId,
-        bucketName,
-        finality: null,
-      },
-    }
-  );
+	const coll = await rendersCollection();
+	await coll.updateOne(
+		{
+			region,
+			username: username.toLowerCase(),
+		},
+		{
+			$set: {
+				renderId,
+				bucketName,
+				finality: null,
+			},
+		}
+	);
 };
 
 export const updateRenderWithFinality = async (
-  renderId: string,
-  username: string,
-  region: AwsRegion,
-  finality: Finality
+	renderId: string,
+	username: string,
+	region: AwsRegion,
+	finality: Finality
 ) => {
-  if (finality && finality.type === "success") {
-    console.log(`Successfully rendered video for ${username}.`);
-  } else {
-    console.log(`Failed to render video for ${username}!`);
-  }
-  const coll = await rendersCollection();
-  return coll.updateOne(
-    {
-      renderId,
-      region,
-    },
-    {
-      $set: {
-        finality: finality,
-      },
-    }
-  );
+	if (finality && finality.type === 'success') {
+		console.log(`Successfully rendered video for ${username}.`);
+	} else {
+		console.log(`Failed to render video for ${username}!`);
+	}
+	const coll = await rendersCollection();
+	return coll.updateOne(
+		{
+			renderId,
+			region,
+		},
+		{
+			$set: {
+				finality: finality,
+			},
+		}
+	);
 };
 
 export const getRender = async (
-  username: string
+	username: string
 ): Promise<WithId<Render> | null> => {
-  const coll = await rendersCollection();
-  const render = await coll.findOne({
-    username: username.toLowerCase(),
-  });
+	const coll = await rendersCollection();
+	const render = await coll.findOne({
+		username: username.toLowerCase(),
+	});
 
-  return render ?? null;
+	return render ?? null;
 };
 
 export const deleteRender = async (render: WithId<Render>) => {
-  const coll = await rendersCollection();
-  await coll.deleteOne({
-    _id: render._id,
-  });
+	const coll = await rendersCollection();
+	await coll.deleteOne({
+		_id: render._id,
+	});
 };
