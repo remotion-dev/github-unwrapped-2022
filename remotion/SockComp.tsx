ad680845cb426d63c2cd5366a2e569ca83812702
Jonny Burger <jonathanburger11@gmail.com>
2022-12-05 23:01:49 +0100

    Update SockComp.tsx

--- remotion/SockComp.tsx
+++ remotion/SockComp.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
-import {AbsoluteFill, spring, useCurrentFrame, useVideoConfig} from 'remotion';
+import {
+	AbsoluteFill,
+	interpolate,
+	spring,
+	useCurrentFrame,
+	useVideoConfig,
+} from 'remotion';
 import {BG_2022} from '../src/palette';
 import {Sock} from './Icons/Sock';
+import {TypeScript} from './Languages/Typescript';
 
 export const SockComp: React.FC = () => {
 	const {fps} = useVideoConfig();
@@ -21,9 +28,17 @@ export const SockComp: React.FC = () => {
 		frame: frame - 10,
 		durationInFrames: 10,
 	});
+	const push = spring({
+		fps,
+		frame: frame - 10,
+		durationInFrames: 20,
+	});
 
 	const scaleX = 1 + squeezeOut * 0.2 - squeezeIn * 0.2;
 	const scaleY = 1 - squeezeOut * 0.2 + squeezeIn * 0.2;
+
+	const top = interpolate(push, [0, 1], [0, -300]);
+	const scaleLogo = interpolate(push, [0, 1], [0.15, 0.3]);
 
 	return (
 		<AbsoluteFill
@@ -31,6 +46,15 @@ export const SockComp: React.FC = () => {
 				backgroundColor: BG_2022,
 			}}
 		>
+			<AbsoluteFill
+				style={{
+					scale: String(scaleLogo),
+					marginLeft: -60,
+					marginTop: top,
+				}}
+			>
+				<TypeScript></TypeScript>
+			</AbsoluteFill>
 			<AbsoluteFill
 				style={{
 					scale: '6',
