fdaedbd6b2f3fc11a2b7f73e8450491cee402891
Jonny Burger <jonathanburger11@gmail.com>
2022-12-06 20:42:14 +0100

    Update Main.tsx

--- remotion/Main.tsx
+++ remotion/Main.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import {AbsoluteFill, Audio, Series} from 'remotion';
+import {BG_2022} from '../src/palette';
 import {AvgCommits} from './AvgCommits';
 import {EndCard} from './EndCard';
 import {EndCard2} from './EndCard2';
 import {IssuesOpened2022} from './IssuesOpened';
 import {CompactStats} from './map-response-to-stats';
+import {Snow} from './Snow';
 import {Socks} from './Socks';
 import {Title} from './Title2022';
 import {TopLang} from './TopLang';
@@ -20,8 +22,13 @@ export const Main: React.FC<{
 	}
 
 	return (
-		<AbsoluteFill>
+		<AbsoluteFill
+			style={{
+				background: BG_2022,
+			}}
+		>
 			<Audio src="https://jonnyburger.s3.eu-central-1.amazonaws.com/the-librarian.mp3"></Audio>
+			<Snow></Snow>
 			<Series>
 				<Series.Sequence durationInFrames={130}>
 					<Title noBackground avatar={stats.avatar}></Title>
