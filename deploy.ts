f61ed220701e06e7006c312817b42e8b7d6c56e8
Jonny Burger <jonathanburger11@gmail.com>
2022-12-08 18:29:48 +0100

    new convention: remotion/index.ts

--- deploy.ts
+++ deploy.ts
@@ -1,52 +1,48 @@
-import {
-  deployFunction,
-  deploySite,
-  getOrCreateBucket,
-} from "@remotion/lambda";
-import dotenv from "dotenv";
-import path from "path";
-import { SITE_ID } from "./src/config";
-import { getAccountCount } from "./src/get-account-count";
-import { usedRegions } from "./src/regions";
-import { setEnvForKey } from "./src/set-env-for-key";
+import {deployFunction, deploySite, getOrCreateBucket} from '@remotion/lambda';
+import dotenv from 'dotenv';
+import path from 'path';
+import {SITE_ID} from './src/config';
+import {getAccountCount} from './src/get-account-count';
+import {usedRegions} from './src/regions';
+import {setEnvForKey} from './src/set-env-for-key';
 dotenv.config();
 
 const count = getAccountCount();
 console.log(`Found ${count} accounts. Deploying...`);
 
 const execute = async () => {
-  for (let i = 1; i <= count; i++) {
-    for (const region of usedRegions) {
-      setEnvForKey(i);
-      const { functionName, alreadyExisted } = await deployFunction({
-        architecture: "arm64",
-        createCloudWatchLogGroup: true,
-        memorySizeInMb: 2048,
-        timeoutInSeconds: 240,
-        region,
-      });
-      console.log(
-        `${
-          alreadyExisted ? "Ensured" : "Deployed"
-        } function "${functionName}" to ${region} in account ${i}`
-      );
-      const { bucketName } = await getOrCreateBucket({ region });
-      const { serveUrl } = await deploySite({
-        siteName: SITE_ID,
-        bucketName,
-        entryPoint: path.join(process.cwd(), "remotion/index.tsx"),
-        region,
-      });
-      console.log(
-        `Deployed site to ${region} in account ${i} under ${serveUrl}`
-      );
-    }
-  }
+	for (let i = 1; i <= count; i++) {
+		for (const region of usedRegions) {
+			setEnvForKey(i);
+			const {functionName, alreadyExisted} = await deployFunction({
+				architecture: 'arm64',
+				createCloudWatchLogGroup: true,
+				memorySizeInMb: 2048,
+				timeoutInSeconds: 240,
+				region,
+			});
+			console.log(
+				`${
+					alreadyExisted ? 'Ensured' : 'Deployed'
+				} function "${functionName}" to ${region} in account ${i}`
+			);
+			const {bucketName} = await getOrCreateBucket({region});
+			const {serveUrl} = await deploySite({
+				siteName: SITE_ID,
+				bucketName,
+				entryPoint: path.join(process.cwd(), 'remotion/index.ts'),
+				region,
+			});
+			console.log(
+				`Deployed site to ${region} in account ${i} under ${serveUrl}`
+			);
+		}
+	}
 };
 
 execute()
-  .then(() => process.exit(0))
-  .catch((err) => {
-    console.error(err);
-    process.exit(1);
-  });
+	.then(() => process.exit(0))
+	.catch((err) => {
+		console.error(err);
+		process.exit(1);
+	});
