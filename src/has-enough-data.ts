079629bff96b679c703922aab1aceec6d3c6bdfb
Jonny Burger <jonathanburger11@gmail.com>
2022-12-13 18:10:48 +0100

    has enough data

--- src/has-enough-data.ts
+++ src/has-enough-data.ts
@@ -1,16 +1,25 @@
-import {CompactStats} from '../remotion/map-response-to-stats';
+import {BackendStats, CompactStats} from '../remotion/map-response-to-stats';
 
 export const hasEnoughData = (stats: CompactStats): boolean => {
 	if (stats.bestCommits.length === 0) {
 		return false;
 	}
-	if (stats.contributionCount === 0) {
-		return false;
-	}
 	if (stats.weekdays.mostCount === 0) {
 		return false;
 	}
+
+	if (!hasEnoughBackendData(stats)) {
+		return false;
+	}
+
+	return true;
+};
+
+export const hasEnoughBackendData = (stats: BackendStats) => {
 	if (stats.repositoriesContributedTo.length === 0) {
+		return false;
+	}
+	if (stats.contributionCount === 0) {
 		return false;
 	}
 
