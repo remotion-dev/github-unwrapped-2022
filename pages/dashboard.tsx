b1ac4e8aad035ab7880e8e15dabd308c037320b4
Jonny Burger <jonathanburger11@gmail.com>
2022-12-19 15:27:08 +0100

    Update dashboard.tsx

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,14 +1,17 @@
 import {useEffect, useState} from 'react';
+import {AbsoluteFill} from 'remotion';
+import {redTheme} from '../remotion/theme';
+import {GithubIcon} from '../src/components/Github';
 
 const Dashboard = () => {
-	const [data, setData] = useState(0);
+	const [data, setData] = useState({renders: 0});
 
 	useEffect(() => {
 		const timeout = setTimeout(() => {
 			fetch('/api/videos')
 				.then((res) => res.json())
 				.then((res) => {
-					setData(res.renders);
+					setData(res);
 				});
 		}, 2000);
 
@@ -18,10 +21,35 @@ const Dashboard = () => {
 	}, [data]);
 
 	return (
-		<div>
-			<h1>Videos rendered:</h1>
-			<h1>{data}</h1>
-		</div>
+		<AbsoluteFill
+			style={{
+				justifyContent: 'center',
+				alignItems: 'center',
+				fontFamily: 'MonaSans',
+				fontSize: 100,
+				lineHeight: 1,
+				fontWeight: 'bold',
+				backgroundColor: redTheme.background,
+			}}
+		>
+			<GithubIcon
+				style={{
+					height: 150,
+					marginBottom: 60,
+				}}
+				theme={redTheme}
+			></GithubIcon>
+			<div
+				style={{
+					fontSize: 50,
+					marginBottom: 20,
+					color: redTheme.mainColor,
+				}}
+			>
+				Videos rendered
+			</div>
+			<div>{data.renders}</div>
+		</AbsoluteFill>
 	);
 };
 
